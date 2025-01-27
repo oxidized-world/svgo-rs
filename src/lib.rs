128cee7d07900fada9c0c31659e791032ce394d9
jacob-lcs <jacob.lcs@qq.com>
2025-01-27 16:22:33 +0800

    fix: reorder imports and clean up plugin initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(clippy::all)]
 use plugins::class_adder::ClassAdderPlugin;
-use process_xml::{XmlPlugin, process_xml};
+use process_xml::{process_xml, XmlPlugin};
 
 use napi_derive::napi;
 
@@ -9,18 +9,15 @@ mod process_xml;
 
 #[napi]
 pub fn optimize(input_xml: String) -> String {
-
-  let mut plugins: Vec<Box<dyn XmlPlugin>> = vec![
-    Box::new(ClassAdderPlugin {
-      target_element: "div".to_string(),
-      class_name: "container".to_string(),
-    }),
-  ];
+  let mut plugins: Vec<Box<dyn XmlPlugin>> = vec![Box::new(ClassAdderPlugin {
+    target_element: "div".to_string(),
+    class_name: "container".to_string(),
+  })];
 
   let processed = process_xml(&input_xml, &mut plugins);
 
   match processed {
     Ok(output) => output,
-    Err(e) => format!("Error: {}", e),  
+    Err(e) => format!("Error: {}", e),
   }
 }
